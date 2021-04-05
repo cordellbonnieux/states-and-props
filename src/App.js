375829bceab4f30a17aab6b83f732bc9400f343a
Cordell <cordellbonnieux@gmail.com>
2021-04-04 17:25:21 -0700

    added another example

--- src/App.js
+++ src/App.js
@@ -6,16 +6,30 @@ class App extends Component {
     super(props);
 
     this.onClickBtn = this.onClickBtn.bind(this);
+
+    this.state = {
+      count:0,
+    };
+
+    this.countUp = this.countUp.bind(this);
   }
 
   onClickBtn() {
     console.log("button has been clicked!");
   }
 
+  countUp() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
   render() {
     return (
       <div>
         <MyComponent title="React" onButtonClicked={this.onClickBtn} />
+        <button onClick={this.countUp}>Click Me Too!</button>
+        <p>{this.state.count}</p>
       </div>
     );
   }
